RevengeOS/android_vendor_revengeos

Add charging sounds
Change-Id: I8d8410afeff193ba90cf337f152c6d871bac49c1

config/common.mk
@@ -82,6 +82,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Charging sounds
+PRODUCT_COPY_FILES += \
+    vendor/carbon/sounds/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
+    vendor/carbon/sounds/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
+
 # Additional packages
 -include vendor/carbon/config/packages.mk
 

StagOS/android_vendor_stag

config/common.mk
@@ -19,4 +19,10 @@ endif
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    vendor/stag/overlay/common
+    vendor/stag/overlay/common \
+    vendor/stag/overlay/themes
+
+# Charging sounds
+PRODUCT_COPY_FILES += \
+    vendor/stag/sounds/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
+    vendor/stag/sounds/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg