fb5d4c7ba866da5012fa0d5960ff3dd41bd43b0f
phenomenall <venkysaihot@gmail.com>
2018-11-06 15:31:57 +0000

    Bringup Stag Tones [2/2] * Also Set our Default tone
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- main.mk
+++ main.mk
@@ -4,3 +4,8 @@ include vendor/stag/config/common.mk
 # Version
 include vendor/stag/config/version.mk
 
+#overrides
+include vendor/stag/config/overrides.mk
+
+# Ringtones
+include frameworks/base/data/sounds/StagAudio.mk
