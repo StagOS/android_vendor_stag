20c8d72ee5c423f34050c9329431e4ad2555c181
vjspranav <pranavasri@live.in>
2018-11-30 20:29:08 +0100

    No Pixel Launcher for now

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -15,16 +15,16 @@
 LOCAL_PATH := $(call my-dir)
 
 # Pixel Launcher
-include $(CLEAR_VARS)
-LOCAL_MODULE := PixelLauncher
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := priv-app/$(LOCAL_MODULE).apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Launcher2 Launcher3 Launcher3QuickStep
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := PixelLauncher
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_SRC_FILES := priv-app/$(LOCAL_MODULE).apk
+#LOCAL_CERTIFICATE := PRESIGNED
+#LOCAL_PRIVILEGED_MODULE := true
+#LOCAL_OVERRIDES_PACKAGES := Launcher2 Launcher3 Launcher3QuickStep
+#LOCAL_MODULE_CLASS := APPS
+#LOCAL_MODULE_SUFFIX := .apk
+#include $(BUILD_PREBUILT)
 
 #ViaBrowser
 include $(CLEAR_VARS)
