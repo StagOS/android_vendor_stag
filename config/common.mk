DirtyUnicorns/android_vendor_du

Compile WeatherClient from source
Change-Id: Ied18d6547ac864c16b266b396e6f07a121a62e0b

config/common.mk
@@ -53,11 +53,16 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc
 
-# Backup Tool
+# Backup tool
 PRODUCT_COPY_FILES += \
     vendor/extras/build/tools/backuptool.sh:install/bin/backuptool.sh \
     vendor/extras/build/tools/backuptool.functions:install/bin/backuptool.functions \
     vendor/extras/build/tools/50-du.sh:system/addon.d/50-du.sh
+
+# Weather client
+PRODUCT_COPY_FILES += \
+    vendor/du/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/du/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 
 # Packages
 include vendor/du/config/packages.mk
@@ -67,9 +72,6 @@ include vendor/du/config/branding.mk
 
 # Themes
 include vendor/themes/common.mk
-
-# Weather
-include vendor/du/config/weather.mk
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common

StagOS/android_vendor_stag

config/common.mk
@@ -35,6 +35,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Launcher3
 
+# Weather client
+PRODUCT_COPY_FILES += \
+    vendor/stag/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/stag/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
+
 #Default Browser
 PRODUCT_PACKAGES += \
     ViaBrowser
@@ -52,6 +57,3 @@ PRODUCT_COPY_FILES += \
     vendor/stag/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml \
     vendor/stag/prebuilt/common/etc/permissions/com.google.android.dialer.support.xml:system/etc/permissions/com.google.android.dialer.support.xml \
     vendor/stag/prebuilt/common/framework/com.google.android.dialer.support.jar:system/framework/com.google.android.dialer.support.jar
-
-# Weather
-include vendor/stag/config/weather.mk