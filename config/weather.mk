DirtyUnicorns/android_vendor_du

Add default permissions
Change-Id: Icba08d4b98452b9d57c771e1719fb002c7019cf5

config/weather.mk
@@ -1,3 +1,4 @@
 PRODUCT_COPY_FILES += \
-    vendor/du/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/du/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/du/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 

StagOS/android_vendor_stag

config/weather.mk
@@ -1,3 +1,4 @@
 PRODUCT_COPY_FILES += \
-    vendor/stag/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/stag/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/stag/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 