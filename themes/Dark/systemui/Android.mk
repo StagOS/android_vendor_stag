4b8f06b65311bb6a3719f786f6fbd14d594a3591
phenomenall <venkysaihot@gmail.com>
2019-03-01 06:08:50 +0100

    Move Dui -> systemui

--- themes/Dark/systemui/Android.mk
+++ themes/Dark/systemui/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := DuiDark
+LOCAL_PACKAGE_NAME := SystemUiDark
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
