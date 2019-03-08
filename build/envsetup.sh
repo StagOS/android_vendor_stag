fc5fca92ca03597684ab254030a8436e13a41b01
vjspranav <pranavasri@live.in>
2019-03-08 16:58:17 +0100

    Make stag and Ascii art
    Signed-off-by: Shashank Patil <9945shashank@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -54,7 +54,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka stag
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
