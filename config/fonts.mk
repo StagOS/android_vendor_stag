SuperiorOS/android_vendor_superior

Vendor: Add samsung font
Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

config/fonts.mk
@@ -1,0 +1,3 @@
+# superior Stuff - Copy to System fonts
+PRODUCT_COPY_FILES += \
+    vendor/superior/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf

StagOS/android_vendor_stag

Change-Id: Ibca19da4cedbdb5f89e733cf97fc73588106ac3c

config/fonts.mk
@@ -1,0 +1,3 @@
+# Stag Stuff - Copy to System fonts
+PRODUCT_COPY_FILES += \
+    vendor/stag/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf