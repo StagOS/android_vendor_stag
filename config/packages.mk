2f75027d2cecfc7d67c23a3ef106075ad3c9e99e
ElDainosor <eldainosor@gmail.com>
2019-06-10 01:07:31 -0300

    QSTiles: Add more styles [3/3]
    > Neon Light: It's a small gradient fading like if the icons were in neon (use your imagination pls)
    > Oxygen: It's a copy of the OOS QS styles, blame simrat39 for this.
    > Triangles: A pyramidal thing, going every time more darker
    > Divided Circle: A small circle with some diagonal dividing
    > Cosmos: An attempt to doing a space
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/packages.mk
+++ config/packages.mk
@@ -254,6 +254,11 @@ PRODUCT_PACKAGES += \
     QStileCookie \
     QStileInkDrop \
     QStileSquaremedo \
+    QStileCosmos \
+    QStileDividedCircle \
+    QStileNeonLight \
+    QStileOxygen \
+    QStileTriangles \
     QStileCircleOutline
 
 # Icon Shapes
