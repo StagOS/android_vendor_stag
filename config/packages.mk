b46bf3b1cba9f57db8e0c38c62f42f87222d0b8c
calebcabob <9941395+calebcabob@users.noreply.github.com>
2019-06-20 05:02:29 +0700

    Add more Switch Styler [3/3]
    Change-Id: Ibe05ed0fdd04b6d21d089d980c7b77d30df8dafc
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -199,7 +199,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MD2Switch \
     OnePlusSwitch \
-    StockSwitch
+    StockSwitch \
+    Contained \
+    Retro \
+    Stockish \
+    Narrow
 
 # Updater
 PRODUCT_PACKAGES += \
