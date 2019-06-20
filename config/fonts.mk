SuperiorOS/android_vendor_superior

vendor:  Add 'GoBold' font family
Required for lockscreen. Also, added in case of adding more fonts.

based on this commit, credits to beanstown and jhenrique
https://gitlab.com/PixelExperience/vendor_pixelstyle/commit/faa3d8365bc1fa0d885e3e98f1e609f6e0fb85fb

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

config/fonts.mk
@@ -1,3 +1,9 @@
 # superior Stuff - Copy to System fonts
 PRODUCT_COPY_FILES += \
+    vendor/superior/prebuilt/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/superior/prebuilt/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/superior/prebuilt/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/superior/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/superior/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/superior/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
     vendor/superior/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf

StagOS/android_vendor_stag

Change-Id: I9ca36b4d0e7899a64405b0221e1589ddfbeb78c3

config/fonts.mk
@@ -1,3 +1,9 @@
 # Stag Stuff - Copy to System fonts
 PRODUCT_COPY_FILES += \
+    vendor/stag/prebuilt/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/stag/prebuilt/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/stag/prebuilt/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/stag/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/stag/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/stag/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
     vendor/stag/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf