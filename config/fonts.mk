SuperiorOS/android_vendor_superior

vendor: Add Neoneon Font
* Thanks @Bootleggers ROM

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

config/fonts.mk
@@ -5,5 +5,6 @@ PRODUCT_COPY_FILES += \
     vendor/superior/prebuilt/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
     vendor/superior/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
     vendor/superior/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
-    vendor/superior/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
+    vendor/superior/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
+	vendor/superior/prebuilt/fonts/neoneon/neoneon.ttf:system/fonts/Neoneon-Regular.ttf \
     vendor/superior/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf

StagOS/android_vendor_stag

Change-Id: Ida45b9d2172fb5470b60bef5d6bf2c2e366fc83e

config/fonts.mk
@@ -7,3 +7,4 @@ PRODUCT_COPY_FILES += \
     vendor/stag/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
     vendor/stag/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
     vendor/stag/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf
+    vendor/stag/prebuilt/fonts/neoneon/neoneon.ttf:system/fonts/Neoneon-Regular.ttf \