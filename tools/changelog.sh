281596c16926209d46d09da236e444efb397bba0
altan <korukaltan@gmail.com>
2019-07-19 18:54:01 +0200

    Add Days to log option Gives an opton to not generate a log

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,5 +1,40 @@
 #!/bin/sh
 
+# Simple sh to automatic generate a file with source and device specif git commit changes to use in a github wiki pages or file.md
+# like this:
+# https://github.com/bhb27/android_vendor_crdroid/blob/change_temp/Changelog.md
+# file.md can work with more data or have more lines then a page wiki
+# input variables set the below the rest must be automatic
+# Specify colors utilized in the terminal
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    ylw=$(tput setaf 3)             #  yellow
+    blu=$(tput setaf 4)             #  blue
+    ppl=$(tput setaf 5)             #  purple
+    cya=$(tput setaf 6)             #  cyan
+    txtbld=$(tput bold)             #  Bold
+    bldred=${txtbld}$(tput setaf 1) #  red
+    bldgrn=${txtbld}$(tput setaf 2) #  green
+    bldylw=${txtbld}$(tput setaf 3) #  yellow
+    bldblu=${txtbld}$(tput setaf 4) #  blue
+    bldppl=${txtbld}$(tput setaf 5) #  purple
+    bldcya=${txtbld}$(tput setaf 6) #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+    rev=$(tput rev) #  Reverse color
+
+if [ -z $days_to_log ];then
+# ask for days and version if not already specified
+echo ""
+echo ${ylw}" ▼ For how many days changelog do you want to generate?"${txtrst}
+echo ""
+echo -e ${blu}" 〉 30/sec Time-out"${txtrst}${red}" ︱ 7 Days by default"${txtrst}
+echo -e "";
+echo -e ${cya}" ▼ Type a number"${txtrst}
+echo -e "";
+# use 'export days_to_log=5' before '. build/envsetup.sh' were 5 is days to log
+read -r -t 30 days_to_log || days_to_log=7
+fi
+
 # Exports
 dir=$ANDROID_BUILD_TOP
 out=$dir/out
@@ -15,7 +50,7 @@ touch $Changelog
 # Print something to build output
 echo ${bldppl}"Generating changelog..."${txtrst}
 
-for i in $(seq 15);
+for i in $(seq $days_to_log);
 do
 export After_Date=`date --date="$i days ago" +%F`
 k=$(expr $i - 1)
