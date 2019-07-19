eedc0c240e66c9df590cc2f1d652995293f0744b
Kshitij Gupta <kshitijgm@gmail.com>
2019-07-19 18:17:57 +0200

    Add Lawnchair as default launcher
    - CI build Lawnchair2-alpha_2319
    - Thanks @deletescape :)

--- config/common.mk
+++ config/common.mk
@@ -50,6 +50,11 @@ PRODUCT_COPY_FILES += \
     vendor/stag/sounds/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
     vendor/stag/sounds/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
 
+#Lawnchair permissions and sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/stag/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/stag/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
 # Weather client
 PRODUCT_COPY_FILES += \
     vendor/stag/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
