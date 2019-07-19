5763e35b65cf55a45895aa4ad97d3bef03f7f9bd
vjspranav <pranavasri@live.in>
2019-07-19 18:05:20 +0200

    Clean Up Vendor

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -14,6 +14,18 @@
 
 LOCAL_PATH := $(call my-dir)
 
+# Lawnchair
+include $(CLEAR_VARS)
+LOCAL_MODULE := Lawnchair
+LOCAL_SRC_FILES := priv-app/Lawnchair.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := Launcher2 Launcher3 Launcher3QuickStep NexusLauncherPrebuilt
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
 #ViaBrowser
 include $(CLEAR_VARS)
 LOCAL_MODULE := ViaBrowser
