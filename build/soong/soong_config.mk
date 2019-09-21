ArrowOS/android_vendor_arrow

vendor: soong: _contents -> _json_contents
Change-Id: I2baa36bbb9aff3a7e70a293ed3352fb718eb611c

build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "Arrow":{$(newline)
+_json_contents := $(_json_contents)    "Arrow":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
@@ -28,6 +28,6 @@ $(call add_json_bool, Uses_qcom_um_4_14_family, $(filter true,$(TARGET_USES_QCOM
 $(call add_json_bool, Target_use_sdclang,                         $(filter true,$(if $(strip $(TARGET_USE_SDCLANG)),true,false)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)

StagOS/android_vendor_stag

build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "Stag":{$(newline)
+_json_contents := $(_json_contents)    "Stag":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
@@ -28,6 +28,6 @@ $(call add_json_bool, Needs_netd_direct_connect_rule, $(filter true,$(TARGET_NEE
 $(call add_json_bool, Needs_camera_boottime_timestamp, $(filter true,$(TARGET_CAMERA_BOOTTIME_TIMESTAMP)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)
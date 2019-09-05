16b482c0e46d2388b8ef80fb587ba0e5ae4c28c5
Alessandro Astone <ales.astone@gmail.com>
2019-09-06 01:13:17 +0200

    vendor: soong: _contents -> _json_contents
    Change-Id: I2baa36bbb9aff3a7e70a293ed3352fb718eb611c

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "Stag":{$(newline)
+_json_contents := $(_json_contents)    "Stag":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
@@ -28,6 +28,6 @@ $(call add_json_bool, Needs_netd_direct_connect_rule, $(filter true,$(TARGET_NEE
 $(call add_json_bool, Needs_camera_boottime_timestamp, $(filter true,$(TARGET_CAMERA_BOOTTIME_TIMESTAMP)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)
