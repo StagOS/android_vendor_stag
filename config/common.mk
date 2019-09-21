whycaf/android_vendor_kcuf

add backup tool from lineage

config/common.mk
@@ -13,3 +13,17 @@ include vendor/kcuf/config/props.mk
 
 # Packages
 include vendor/kcuf/config/packages.mk
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/kcuf/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/kcuf/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/kcuf/prebuilt/common/bin/50-kcuf.sh:system/addon.d/50-kcuf.sh \
+    vendor/kcuf/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/kcuf/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/kcuf/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/kcuf/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif

StagOS/android_vendor_stag

config/common.mk
@@ -28,3 +28,17 @@ PRODUCT_COPY_FILES += \
 
 #IgnoreNeverallows
 SELINUX_IGNORE_NEVERALLOWS := true
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/stag/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/stag/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/stag/prebuilt/common/bin/50-stag.sh:system/addon.d/50-stag.sh \
+    vendor/stag/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/stag/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/stag/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/stag/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif