cb248fa57330523607d71eefa1c6bcf44103df85
vjspranav <pranavasri@live.in>
2019-09-21 23:28:01 +0200

    Initial stag vendor

--- config/common.mk
+++ config/common.mk
@@ -4,6 +4,15 @@ PRODUCT_BRAND := stag
 PRODUCT_DEVICE := generic
 EXCLUDE_SYSTEMUI_TESTS := true
 
+# version
+include vendor/stag/config/version.mk
+
+# Props
+include vendor/stag/config/props.mk
+
+# Packages
+include vendor/stag/config/packages.mk
+
 # Bootanimation
 ifeq ($(TARGET_SCREEN_RES), 720)
 	PRODUCT_COPY_FILES += vendor/stag/prebuilt/common/bootanimation/720p.zip:system/media/bootanimation.zip
--- main.mk
+++ main.mk
@@ -3,3 +3,4 @@ include vendor/stag/config/common.mk
 
 # Version
 include vendor/stag/config/version.mk
+
