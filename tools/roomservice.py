orchidnpsg/android_build

roomservice: change default_manifest
Change-Id: I2064691fa5ea1f4329515a360d68f35e5b3315f1

tools/roomservice.py
@@ -41,7 +41,7 @@ except ImportError:
     urllib.request = urllib2
 
 DEBUG = False
-default_manifest = ".repo/manifest.xml"
+default_manifest = ".repo/manifests/snippets/aim.xml"
 
 custom_local_manifest = ".repo/local_manifests/aim_manifest.xml"
 custom_default_revision = "o8.1"

StagOS/android_vendor_stag

Signed-off-by: Ravi Pingat <pingatravi@gmail.com>

tools/roomservice.py
@@ -41,7 +41,7 @@ except ImportError:
     urllib.request = urllib2
 
 DEBUG = False
-default_manifest = ".repo/manifest.xml"
+default_manifest = ".repo/manifests/snippets/stag.xml"
 
 custom_local_manifest = ".repo/local_manifests/stag_manifest.xml"
 custom_default_revision = "sx"