PotatoProject/vendor_potato

Don't enforce rro for overlays
Change-Id: Icc5f38acaf22fced599f242b8fe526621da8c9ce

config/common.mk
@@ -149,6 +149,7 @@ ADDITIONAL_FONTS_FILE := vendor/potato/prebuilt/common/fonts/google-sans.xml
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/potato/overlay/common
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/potato/overlay/common
 
 # Packages
 include vendor/potato/config/packages.mk

StagOS/android_vendor_stag

config/common.mk
@@ -30,6 +30,7 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/stag/overlay/common \
     vendor/stag/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/stag/overlay/common
 
 # Changelog
 #PRODUCT_COPY_FILES += \