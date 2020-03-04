f70a82da610fd7f0d12708aeb2842a1bce2d57ce
z3DD3r <z3dd3r@gmail.com>
2020-03-04 12:42:10 +0300

    backuptool: Properly unmount system partition
    For non AB devices system partition should be unmounted
    if check_prereq function fails.
    
    This patch also refactors backuptool a bit for AB devices
    in order to look same as backuptool for non AB devices.
    
    Change-Id: Ia1f4ae95c9e4dae4df844853e81c264bc838f177

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -33,9 +33,9 @@ restore_addon_d() {
 check_prereq() {
 # If there is no build.prop file the partition is probably empty.
 if [ ! -r $S/build.prop ]; then
-    exit 127
+    return 0
 fi
-
+return 1
 }
 
 # Execute /system/addon.d/*.sh scripts with $1 parameter
@@ -77,7 +77,10 @@ case "$1" in
   backup)
     mount_system
     mkdir -p $C
-    check_prereq
+    if ! check_prereq; then
+      unmount_system
+      exit 127
+    end
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
@@ -86,7 +89,10 @@ case "$1" in
   ;;
   restore)
     mount_system
-    check_prereq
+    if ! check_prereq; then
+      unmount_system
+      exit 127
+    end
     run_stage pre-restore
     run_stage restore
     run_stage post-restore
