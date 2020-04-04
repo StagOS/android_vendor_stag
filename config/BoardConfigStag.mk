412c2cea362ceef981704d8b52e0115dd675e6d3
rpingat <pingatravi@gmail.com>
2020-04-04 14:15:11 +0530

    vendor: Fix derp

--- config/BoardConfigStag.mk
+++ config/BoardConfigStag.mk
@@ -11,8 +11,8 @@ endif
 
 include vendor/stag/config/BoardConfigSoong.mk
 
-ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true) 
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
 PRODUCT_SOONG_NAMESPACES += \
-    frameworks/av/camera/cameraserver \ 
+    frameworks/av/camera/cameraserver \
     frameworks/av/services/camera/libcameraservice
 endif
