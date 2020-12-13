296694eaadd4689818f8fa756081055d33546912
vjspranav <pranavasri@live.in>
2020-12-13 15:14:46 +0100

    Build updater for all

--- config/ota.mk
+++ config/ota.mk
@@ -1,6 +1,6 @@
-ifeq ($(STAG_BUILD_TYPE), OFFICIAL)
+#ifeq ($(STAG_BUILD_TYPE), OFFICIAL)
 
-STAG_OTA_VERSION_CODE := ten
+STAG_OTA_VERSION_CODE := eleven
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.stag.ota.version_code=$(STAG_OTA_VERSION_CODE)
@@ -11,4 +11,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/stag/config/permissions/com.stag.updater.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.stag.updater.xml
 
-endif
+#endif
