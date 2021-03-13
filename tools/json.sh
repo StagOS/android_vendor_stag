a27ba48e9f850764ad6f3bcacfac8cf6b1c05a5b
vjspranav <pranavasri@live.in>
2021-03-13 19:14:24 +0100

    add back OTA Gen script

--- tools/json.sh
+++ tools/json.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "{ \"response\": [ { \"datetime\": $(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2), \"filename\": \"$(basename $(ls $OUT/StagOS*.zip))\", \"id\": \"$((sha256sum $OUT/StagOS*.zip) | cut -d ' ' -f1)\", \"romtype\": $(grep ro\.stag\.releasetype $OUT/system/build.prop | cut -d= -f2), \"size\": $(stat -c%s $OUT/StagOS*.zip), \"url\": \"(Yourwebsite)/$(basename $(ls $OUT/StagOS*.zip))\", \"version\": $(grep ro\.stag\.version $OUT/system/build.prop | cut -d= -f2) " | tee $OUT/update
+device=${TARGET_PRODUCT#*_}
+OUT="$OUT_DIR/target/product/$device"
+echo "{ \"response\": [ { \"datetime\": $(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2), \"filename\": \"$(basename $(ls $OUT/StagOS*.zip))\", \"id\": \"$((sha256sum $OUT/StagOS*.zip) | cut -d ' ' -f1)\", \"size\": $(stat -c%s $OUT/StagOS*.zip), \"url\": \"https://releases.stag-os.workers.dev/${device}/$(basename $(ls $OUT/StagOS*.zip))\", \"version\": \"$(grep ro\.stag\.version $OUT/system/build.prop | cut -d= -f2)\" }]}" | tee $OUT/update
