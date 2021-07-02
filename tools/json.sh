0b43b5aa744d1d4cf343dbabdc18105ddb88f1a1
Vjs Pranavasri <pranavasri@live.in>
2021-07-02 17:04:12 +0530

    Get build version for json

--- tools/json.sh
+++ tools/json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 device=${TARGET_PRODUCT#*_}
 OUT="$OUT_DIR/target/product/$device"
-echo "{ \"response\": [ { \"datetime\": $(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2), \"filename\": \"$(basename $(ls $OUT/StagOS*.zip))\", \"id\": \"$((sha256sum $OUT/StagOS*.zip) | cut -d ' ' -f1)\", \"size\": $(stat -c%s $OUT/StagOS*.zip), \"url\": \"https://releases.stag-os.workers.dev/${device}/$(basename $(ls $OUT/StagOS*.zip))\", \"version\": \"$(grep ro\.stag\.version $OUT/system/build.prop | cut -d= -f2)\" }]}" | tee $OUT/update
+echo "{ \"response\": [ { \"datetime\": $(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2), \"filename\": \"$(basename $(ls $OUT/StagOS*.zip))\", \"id\": \"$((sha256sum $OUT/StagOS*.zip) | cut -d ' ' -f1)\", \"size\": $(stat -c%s $OUT/StagOS*.zip), \"url\": \"https://releases.stag-os.workers.dev/${device}/$(basename $(ls $OUT/StagOS*.zip))\", \"version\": \"$(grep ro\.stag\.build\.version $OUT/system/build.prop | cut -d= -f2)\" }]}" | tee $OUT/update
