26ea171f719ef1d89dcd1215c472b71ac43447e5
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-10-07 12:53:56 -0400

    vendor: Set Google Sans Text as the default body font.
    * With Android 12, Google Sans is now a variable font type as in
      the font (.ttf) file now contains various different font glyphs
      that the system can use when it requests for a particular weight
      and OPSZ axis values. Google Sans Text - Google's new Roboto body
      font companion is also bundled in this font file.
    
    * To make use of the different glyphs stored in the font file we
      define the OPSZ and weight of the fonts to inline with what Pixels
      have thus, giving us the same look and feel.
    
    * Change the framework font overlay to reflect the new font alias we
      created in `font_customization.xml`
    
    Change-Id: I48a747c88a23e1b8c8e20b762a6f403b8d6c4637
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/5749
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,9 +1,6 @@
 # Fonts
 PRODUCT_COPY_FILES += \
     vendor/stag/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-    vendor/stag/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/stag/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
     vendor/stag/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/stag/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
-    vendor/stag/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
     vendor/stag/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
