8cb93531ae16b45a6de91791c0490cb5cf998464
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-11-09 19:53:05 +0200

    vendor: soong: Add camera_needs_client_info_lib
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -41,6 +41,7 @@ SOONG_CONFIG_NAMESPACES += stagGlobalVars
 SOONG_CONFIG_stagGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info_lib \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -80,6 +81,7 @@ SOONG_CONFIG_stagQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_stagGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_stagGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_stagGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_stagGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
