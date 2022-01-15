4e1797ef648eaa5f99437bbe6b28149f3aabc83f
vjspranav <pranavasri@live.in>
2022-01-16 01:00:25 +0530

    Use release-keys for all builds

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,8 +1,6 @@
 # StagOS System Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-#    ro.stag.version=$(STAG_VERSION) \
     ro.stag.releasetype=$(STAG_BUILD_TYPE) \
-#    ro.stag.build.version=$(StagOS_VERSION).$(StagOS_BUILD) \
     ro.modversion=$(STAG_VERSION) \
 
 # StagOS Platform Display Version
@@ -20,12 +18,8 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
 # Build fingerprint
 ifeq ($(BUILD_FINGERPRINT),)
 BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
-ifneq ($(filter OFFICIAL,$(STAG_BUILD_TYPE)),)
 CUSTOM_DEVICE ?= $(TARGET_DEVICE)
 BUILD_SIGNATURE_KEYS := release-keys
-else
-BUILD_SIGNATURE_KEYS := test-keys
-endif
 BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(CUSTOM_DEVICE)/$(CUSTOM_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER_CUSTOM):$(TARGET_BUILD_VARIANT)/$(BUILD_SIGNATURE_KEYS)
 endif
 ADDITIONAL_SYSTEM_PROPERTIES += \
