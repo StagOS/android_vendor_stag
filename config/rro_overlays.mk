9462d636a2b1a0241825164cfa2550d83316da6e
vjspranav <pranavasri@live.in>
2022-02-09 18:41:01 +0530

    Remove overlay

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -25,7 +25,6 @@ PRODUCT_PACKAGES += \
     PixelSetupWizardOverlayActiveEdge \
     PixelFwResOverlay \
     GoogleConfigOverlay \
-#    SystemUIGoogleOverlay
     GesturalNavigationOverlayLong \
     GesturalNavigationOverlayMedium \
    NavigationBarNoHintOverlay
