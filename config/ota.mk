a235958453dee9d312e537c0d05930cf717ab626
vjspranav <pranavasri@live.in>
2022-03-05 23:12:50 +0530

    Do not allow downgrading

--- config/ota.mk
+++ config/ota.mk
@@ -4,7 +4,7 @@ STAG_OTA_VERSION_CODE := 12.0
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.stag.ota.version_code=$(STAG_OTA_VERSION_CODE) \
-    stag.updater.allow_downgrading=true
+#    stag.updater.allow_downgrading=true
 
 PRODUCT_PACKAGES += \
     Updater
