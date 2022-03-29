194fad78606cdc1ac090b82b35987761790fc02a
vjspranav <pranavasri@live.in>
2022-03-29 18:18:26 +0530

    12.1

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-StagOS_VERSION = 12.0
+StagOS_VERSION = 12.1
 
 STAG_BASE_VERSION = $(StagOS_VERSION)
 
