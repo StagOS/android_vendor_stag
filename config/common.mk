d80c63e323867630513a6b56d95f38a189e1c6c9
someone5678 <nemui3353@gmail.com>
2022-11-25 14:29:55 +0900

    vendor: Build BtHelper
    Change-Id: Ic196ffe2a59de2a189478da16b7ba1b5a7393e45

--- config/common.mk
+++ config/common.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/stag/prebuilt/common/etc/init/init.stag-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.stag-updater.rc
 
+# BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
