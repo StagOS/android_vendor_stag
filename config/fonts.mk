bb5d47f11b8134b7a1d30dff7ff70c8260f88795
Anay Wadhera <anay1018@gmail.com>
2022-12-09 12:51:26 +0900

    vendor: Adapt clock font to qpr1
    Change-Id: Ie369cdc9e1f491e2326036e90270c625b04452b6
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/9118
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-by: Sourajit Karmakar <sourajitk@statixos.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -2,5 +2,5 @@
 PRODUCT_COPY_FILES += \
     vendor/stag/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/stag/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/stag/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
     vendor/stag/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
-
