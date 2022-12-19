a6577673d3e47299a38ebc8113a06017da6e2472
vjspranav <pranavasri@live.in>
2022-12-19 19:00:56 +0000

    [PARTIAL Revert] "kernel: Use nproc from prebuilts" * Remove variables
    This reverts commit f92e11a4315709d84abebc7701813ebf6c970331.

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -122,14 +122,11 @@ ifeq ($(KERNEL_ARCH),arm64)
    KERNEL_CROSS_COMPILE += CROSS_COMPILE_COMPAT="$(KERNEL_TOOLCHAIN_arm)/$(KERNEL_TOOLCHAIN_PREFIX_arm)"
 endif
 
-prebuilt_build_tools_stag := prebuilts/tools-stag
-prebuilt_build_tools_stag_bin := $(prebuilt_build_tools_stag)/$(HOST_PREBUILT_TAG)/bin
-
 # Clear this first to prevent accidental poisoning from env
 KERNEL_MAKE_FLAGS :=
 
 # Add back threads, ninja cuts this to $(nproc)/2
-KERNEL_MAKE_FLAGS += -j$(shell $(prebuilt_build_tools_stag_bin)/nproc --all)
+KERNEL_MAKE_FLAGS += -j$(shell prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin/nproc --all)
 
 ifeq ($(TARGET_KERNEL_CLANG_COMPILE),false)
   ifeq ($(KERNEL_ARCH),arm)
@@ -150,14 +147,14 @@ else
 endif
 
 TOOLS_PATH_OVERRIDE := \
-    LD_LIBRARY_PATH=$(BUILD_TOP)/$(prebuilt_build_tools_stag)/$(HOST_PREBUILT_TAG)/lib:$$LD_LIBRARY_PATH \
-    PERL5LIB=$(BUILD_TOP)/$(prebuilt_build_tools_stag)/common/perl-base
+    LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/lib:$$LD_LIBRARY_PATH \
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-lineage/common/perl-base
 
 ifeq ($(KERNEL_ARCH),arm64)
   # Add 32-bit GCC to PATH so that arm-linux-androidkernel-as is available for CONFIG_COMPAT_VDSO
-  TOOLS_PATH_OVERRIDE += PATH=$(BUILD_TOP)/$(prebuilt_build_tools_stag)/$(HOST_PREBUILT_TAG)/bin:$(KERNEL_TOOLCHAIN_arm):$$PATH
+  TOOLS_PATH_OVERRIDE += PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin:$(KERNEL_TOOLCHAIN_arm):$$PATH
 else
-  TOOLS_PATH_OVERRIDE += PATH=$(BUILD_TOP)/$(prebuilt_build_tools_stag)/$(HOST_PREBUILT_TAG)/bin:$$PATH
+  TOOLS_PATH_OVERRIDE += PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin:$$PATH
 endif
 
 # Set DTBO image locations so the build system knows to build them
