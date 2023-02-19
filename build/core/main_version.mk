64b743937c7fe4c2fcba86472d87481d2883727a
Karan Parashar <karan@pixelos.net>
2023-02-19 20:47:01 +0000

    vendor: ota: Inline with our updater
    [1]: PixelOS-AOSP/packages_apps_Updates@10d53fd86d36b7299bd01aa22e210f886f46cc78
    [2]: PixelOS-AOSP/packages_apps_Settings@2487f236e9ed62c633769152b99ba6139f6eb794
    
    Change-Id: I7aa2b98c11548d544aaa9954946f094ef9c90be6
    
    Signed-off-by: Shashank Patil <9945shashank@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -7,13 +7,14 @@ endif
 # StagOS Platform Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.stag.build.date=$(BUILD_DATE) \
+    ro.stag.device=$(STAG_BUILD) \
     ro.stag.fingerprint=$(ROM_FINGERPRINT) \
     ro.stag.version=$(STAG_VERSION) \
-    ro.stag.device=$(STAG_BUILD) \
     ro.modversion=$(STAG_VERSION)
 
 # Updater
 ifeq ($(IS_OFFICIAL),true)
     ADDITIONAL_SYSTEM_PROPERTIES  += \
-        ro.is_official=true
+        net.stagos.build_type=stag_admin \
+        net.stagos.version=$(STAG_VERSION_PROP)
 endif
