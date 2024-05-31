0b53eb46f649a2b19e0646206f3aa05586996ece
vjspranav <pranavasri@live.in>
2024-05-31 23:24:03 +0530

    Build clocks * We have them both on gapps and pristine builds

--- main.mk
+++ main.mk
@@ -10,6 +10,9 @@ include vendor/stag/config/version.mk
 # overrides
 include vendor/stag/config/overrides.mk
 
+# Clocks
+$(call inherit-product-if-exists, vendor/SystemUIClocks/product.mk)
+
 # Themes
 #$(call inherit-product, vendor/stag/config/themes.mk)
 
