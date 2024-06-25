c8a314ac7c03c8ff5e0c892bc5f428b5cc54c711
Andrzej Perczak <linux@andrzejperczak.com>
2024-06-26 00:01:23 +0200

    Add fag for enabling blur * Properly disable blur on app launch 	ro.launcher.blur.appLaunch is 1 by default * Enable blur by default

--- config/common.mk
+++ config/common.mk
@@ -269,6 +269,16 @@ PRODUCT_PACKAGES += \
     UdfpsResources
 endif
 
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+endif
+
 # Quick Tap
 ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
 PRODUCT_COPY_FILES += \
