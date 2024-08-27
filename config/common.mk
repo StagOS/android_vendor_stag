PixelOS-AOSP/vendor_aosp

common: Conditionally build Updater on official builds
Change-Id: I821e377c36e1426134e1c68fb23824afb3402e43

config/common.mk
@@ -160,13 +160,6 @@ endif
 # Bootanimation
 include vendor/aosp/config/bootanimation.mk
 
-# PixelOS packages
-PRODUCT_PACKAGES += \
-    Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc \
-
 # Charger
 PRODUCT_PACKAGES += \
     product_charger_res_images
@@ -274,6 +267,9 @@ include vendor/aosp/config/version.mk
 # TouchGestures
 PRODUCT_PACKAGES += \
     TouchGestures
+
+# OTA
+$(call inherit-product, vendor/aosp/config/ota.mk)
 
 # Pixel Framework
 $(call inherit-product, vendor/pixel-framework/config.mk)

config/ota.mk
@@ -1,0 +1,8 @@
+# Updater
+ifeq ($(IS_OFFICIAL),true)
+    PRODUCT_PACKAGES += \
+        Updater
+
+    PRODUCT_COPY_FILES += \
+        vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
+endif

StagOS/android_vendor_stag

Signed-off-by: Shashank Patil <9945shashank@gmail.com>

config/common.mk
@@ -111,13 +111,6 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(STAG_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
-# StagOS packages
-PRODUCT_PACKAGES += \
-    Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/stag/prebuilt/common/etc/init/init.stag-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.stag-updater.rc
-
 # BtHelper
 PRODUCT_PACKAGES += \
     BtHelper
@@ -243,5 +236,8 @@ else
 endif
 
 include vendor/stag/config/version.mk
+
+# OTA
+$(call inherit-product, vendor/stag/config/ota.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

config/ota.mk
@@ -1,0 +1,8 @@
+# Updater
+ifeq ($(IS_OFFICIAL),true)
+    PRODUCT_PACKAGES += \
+        Updater
+
+    PRODUCT_COPY_FILES += \
+        vendor/stag/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
+endif