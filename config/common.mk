PixelOS-AOSP/vendor_aosp

vendor: Remove useless addon.d file
* Avoid detection as custom ROM

Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf

config/common.mk
@@ -30,11 +30,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/aosp/prebuilt/common/bin/50-custom.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-custom.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-custom.sh
+    vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \

StagOS/android_vendor_stag

config/common.mk
@@ -30,11 +30,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/stag/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/stag/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/stag/prebuilt/common/bin/50-stag.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-stag.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-stag.sh
+    vendor/stag/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \